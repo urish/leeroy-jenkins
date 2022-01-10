c77a0b19060004c4a9957efd3812cae7ce0e5268
Uri Shaked <uri@urishaked.com>
2022-01-10 11:44:40 +0200

    feat: add authentication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { FirebaseAppProvider } from 'reactfire';
+import '../styles/globals.css';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDdXNU0S4DbGSW6Kj08rqgZZtgtAYw17zo',
+  authDomain: 'leeroy-jenkins-38f35.firebaseapp.com',
+  projectId: 'leeroy-jenkins-38f35',
+  storageBucket: 'leeroy-jenkins-38f35.appspot.com',
+  messagingSenderId: '687723673209',
+  appId: '1:687723673209:web:3b9e9d20d864264516c6a8',
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+      <Component {...pageProps} />
+    </FirebaseAppProvider>
+  );
 }
 
 export default MyApp;
