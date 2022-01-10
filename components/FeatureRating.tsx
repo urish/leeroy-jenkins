671e82b1fa47196cdf2172a20e34fed1136b4732
Uri Shaked <uri@urishaked.com>
2022-01-10 12:04:30 +0200

    feat: add firestore integration

--- components/FeatureRating.tsx
+++ components/FeatureRating.tsx
@@ -1,9 +1,23 @@
-const features = [
-  { title: 'Magic smoke' },
-  { title: 'Breadboard (מטריצה) 🕯️' },
-];
+import { collection, query } from 'firebase/firestore';
+import { useFirestore, useFirestoreCollectionData } from 'reactfire';
 
 export function FeatureRating() {
+  const firestore = useFirestore();
+  const featuresCollection = collection(firestore, 'features');
+  const featuresQuery = query(featuresCollection);
+
+  // ReactFire!
+  const { status, data: features } = useFirestoreCollectionData(featuresQuery, {
+    idField: 'id', // this field will be added to the object created from each document
+  });
+
+  if (status === 'loading') {
+    return <div>Loading data...</div>;
+  }
+  if (status === 'error') {
+    return <div>Error connecting to database.</div>;
+  }
+
   return (
     <ul>
       {features.map(({ title }) => (
